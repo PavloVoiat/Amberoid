12dc075e012ceab0700ed52495930f8a479d1e8d
Pavlo Voiat <voiatpavlo1@gmail.com>
2026-08-23 17:59:41 +0200

    Text marquee at song info

--- app/src/main/java/com/pavo/amberoid/ui/components/TrackInfo.kt
+++ app/src/main/java/com/pavo/amberoid/ui/components/TrackInfo.kt
@@ -2,14 +2,12 @@ package com.pavo.amberoid.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.basicMarquee
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
